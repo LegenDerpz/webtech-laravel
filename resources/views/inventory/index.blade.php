91678184707d9ce5d59faa66efad85f9d4f19182
FranzDV <s2121760@usls.edu.ph>
2025-02-04 11:06:22 +0800

    Created inventory table

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    This is the inventory page.
+   <div class="container">
+        <br><br><br><br>
+        <div class="row">
+
+        <div class="card">
+            <div class="card-body">
+                <table class="table">
+    
+                </table>
+            </div>
+        </div>
+   </div>
 </body>
+<script src = https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js>
 </html>
